7b43f2adb3c5665bf0479b79117cff49425d83de
Shakespeare89 <growingupcollegestyle@gmail.com>
2020-09-12 17:08:08 -0600

    tad project

--- src/App.js
+++ src/App.js
@@ -1,25 +1,51 @@
 import React from 'react';
-import logo from './logo.svg';
+import logo from './logo.png';
 import './App.css';
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { NavigationBar } from './components/NavigationBar';
+
+
+import { Contact } from './Contact';
+import { Home } from './Home';
+import { About } from './About';
+
+import Sidebar from './components/Sidebar';
+import { Order } from './Order';
+
+import { FooterBar } from './components/Footer';
+
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    
+    <React.Fragment>
+      <Router>
+        <NavigationBar>
+          <i class="fas fa-dragon"></i> 
+        </NavigationBar>
+        
+     
+
+        <Sidebar />
+
+     
+
+
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route path="/about" component={About} />
+          <Route path="/order" component={Order} />
+          <Route path="/contact" component={Contact} />
+         
+        </Switch>
+      </Router>
+      <FooterBar />
+      
+    </React.Fragment>
+
+   
   );
 }
 
